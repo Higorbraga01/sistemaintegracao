2ab1721262c902d6a5a8e07971bb4c7aefc7a647
Higor Braga <higorbraga00@gmail.com>
2025-06-23 17:51:26 -0300

    feat: inicia projeto.

---
+++ src/main/java/com/example/sistemaintegracao/SistemaintegracaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaintegracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaintegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaintegracaoApplication.class, args);
+	}
+
+}
